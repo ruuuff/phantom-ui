4f052902def6e7dc4942a0c63f605d38f3fd349e
Carlos Daniel <ruuufffaria@gmail.com>
2021-03-18 16:17:48 -0300

    Dark theme

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 // Menu
-function toggleMenu() {
+function toggleMenu(e) {
+  e.preventDefault()
   const menuOverlay = document.getElementById('menu-overlay')
   menuOverlay.classList.toggle('show')
+}
+
+function changeTheme(e) {
+  e.preventDefault()
+
+  const html = document.getElementsByTagName('html')[0]
+  const themeBtn = document.getElementById('theme')
+
+  html.classList.toggle('dark')
+
+  if (html.classList.contains('dark')) {
+    themeBtn.innerHTML = 'Theme <i class="fas fa-moon"></i>'
+  } else {
+    themeBtn.innerHTML = 'Theme <i class="fas fa-sun"></i>'
+  }
 }
